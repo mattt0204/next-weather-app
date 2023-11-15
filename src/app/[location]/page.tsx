822f1f1e6cd5363086da53f546820fbd18838dac
matt <wootae.jung@beautyselection.co.kr>
2023-11-15 14:28:20 +0900

    feat : index 페이지 상세페이지 notfound 페이지 생성

---
+++ src/app/[location]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function DetailPage() {
+  return (
+    <>
+      <h1>상세 페이지에서는 서울의 3일 날씨를 나타냅니다.</h1>
+      <ul>
+        <li>서울의 날씨는 11월 15일 섭씨 10도입니다.</li>
+        <li>서울의 날씨는 11월 15일 섭씨 10도입니다.</li>
+        <li>서울의 날씨는 11월 15일 섭씨 10도입니다.</li>
+      </ul>
+      <button>홈으로</button>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default function Home() {
-  return <></>
+  return (
+    <div>
+      <h1>날씨앱 만들기 🌈</h1>
+      <button>캐시 비우기</button>
+      <ul>
+        <li>서울의 날씨는 sunny</li>
+        <li>홍콩의 날씨는 sunny</li>
+      </ul>
+    </div>
+  )
 }
